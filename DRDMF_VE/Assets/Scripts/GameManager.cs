78de793375f9fde9854b9eca140eea9e1c1839a4
paulcrepin <mail@paulcrepin.com>
2021-01-26 02:52:11 +0100

    Update : Expe V2

---
+++ DRDMF_VE/Assets/Scripts/GameManager.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    #region VARIABLES
+
+    // numéro de participant
+    public int subjectNumber = 0;
+    // nombre d'essai pendant les essais actifs
+    public int numberOfTrials = 5;
+    // the box
+    public GameObject box;
+    // color for the box
+    public Material greenBox;
+    public Material greyBox;
+    public Material redBox;
+    // animator correspondant aux mouvement aller et retour du cube
+    public Animator MooveAnim;
+    // variable indiquant quand le mode pause (aucune action possible) est activé
+    public bool playerPauseState;
+    // var indiquant que l'action de pousser le cube est attendu (passage du cube en couleur verte)
+    private bool activMode_Action;
+    // var indiquant que la variable à changé
+    private long timeModeStarted;
+    // Nombre d'essai en cour
+    private int ActivTrialNumber = 0;
+    // Variable permettant d'enregistrer et récupérer la position de l'animation lors d'un nouvel input
+    float animPosition = 0;
+    // Var recceuillant les logs
+    string mylogs = "";
+    #endregion
+
+    #region UNITY FUNCTIONS
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerPauseState = true;
+        box.GetComponent<Renderer>().material = greyBox;
+        writeLogs("Initialisation de session");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(!playerPauseState)
+        {
+            AnimatorStateInfo animationStateInfo = MooveAnim.GetCurrentAnimatorStateInfo(0);
+
+            if (Input.GetKeyDown("e"))
+            {
+                Debug.Log("HERE");
+                AnimIsTriggerd(animationStateInfo);
+            }
+            if (Input.GetKeyUp("e"))
+            {
+                AnimIsStopped(animationStateInfo);
+            }
+            
+            if ((DateTimeOffset.Now.ToUnixTimeSeconds() - timeModeStarted) > 3)
+            {
+                if (ActivTrialNumber < numberOfTrials*2)
+                {
+                    if (activMode_Action)
+                    {
+                        Debug.Log("Inhib");
+                        setActivInhibitionMode();
+                    }
+                    else
+                    {
+                        Debug.Log("Action");
+                        setActivActionMode();
+                    }
+                }
+                else
+                {
+                    Debug.Log("Pause");
+                    setPauseMode();
+                }
+            }
+        }
+        else if (Input.GetKeyDown("space"))
+        {
+            if (playerPauseState)
+            {
+                setActivMode();
+            }
+        }
+    }
+
+    #endregion
+
+    #region FUNCTIONS  
+
+    public void writeLogs(string newlog)
+    {
+        System.DateTime currentTime = DateTime.Now;
+
+        int hour = currentTime.Hour;
+        int minute = currentTime.Minute;
+        int second = currentTime.Second;
+        int millisecond = currentTime.Millisecond;
+        string recordedEntry = hour.ToString() + ":" + minute.ToString() + ":" + second.ToString() + ":" + millisecond.ToString();
+        mylogs = mylogs + "\n" + recordedEntry + " ----> " + newlog;
+        System.IO.File.WriteAllText("MyLogsFile"+ subjectNumber +".txt", mylogs);
+    }
+
+    private void setActivMode()
+    {
+        ActivTrialNumber = 0;
+        writeLogs("__ActivMode__");
+        playerPauseState = false;
+        setActivActionMode();
+    }
+    private void setActivActionMode()
+    {
+        ActivTrialNumber ++;
+        timeModeStarted = DateTimeOffset.Now.ToUnixTimeSeconds();
+        box.GetComponent<Renderer>().material = greenBox;
+        writeLogs(" :: > Action");
+        activMode_Action = true;
+    }
+    private void setActivInhibitionMode()
+    {
+        ActivTrialNumber ++;
+        timeModeStarted = DateTimeOffset.Now.ToUnixTimeSeconds();
+        box.GetComponent<Renderer>().material = redBox;
+        writeLogs(" :: > Inhibition");
+        activMode_Action = false;
+    }
+    private void setPauseMode()
+    {
+        ActivTrialNumber=0;
+        writeLogs("__PauseMode__");
+        playerPauseState = true;
+        box.GetComponent<Renderer>().material = greyBox;
+        // accéder aux info de l'animator
+        AnimatorStateInfo animationState = MooveAnim.GetCurrentAnimatorStateInfo(0);
+        if (animationState.IsName("MoveForward"))
+        {
+            animPosition = 1 - animationState.normalizedTime;
+            if (animPosition < 0)
+                animPosition = 0;
+            else
+                animPosition = 0;
+            GetComponent<Animator>().Play("MoveBackward", 0, animPosition);
+        }
+    }
+    private void AnimIsTriggerd(AnimatorStateInfo animationState)
+    {
+        writeLogs(" < Trigger activated >");
+        if (animationState.IsName("MoveBackward"))
+        {
+            // identifier la position de l'animation précédente (nécessité d'inverser la position [avec le 1-position précédente] car les animation sont en miroir)
+            animPosition = 1 - animationState.normalizedTime;
+            // Si la position est inférieur à 0, initialiser la position à 0 (= position initiale de l'animation qui correspond à la position maximum de la précédente animation)
+            if (animPosition < 0)
+                animPosition = 0;
+        }
+        else
+            animPosition = 0;
+        // Jouer l'animation Forward en initialisant animPosition
+        GetComponent<Animator>().Play("MoveForward", 0, animPosition);
+    }
+    private void AnimIsStopped(AnimatorStateInfo animationState)
+    {
+        writeLogs(" < Trigger desactivated >");
+        if (animationState.IsName("MoveForward"))
+        {
+            animPosition = 1 - animationState.normalizedTime;
+            if (animPosition < 0)
+                animPosition = 0;
+        }
+        else
+            animPosition = 0;
+        GetComponent<Animator>().Play("MoveBackward", 0, animPosition);
+    }
+
+    #endregion
+
+
+
+
+
+}
